b097a3fced70a203a5d93a04e0d83eaf2e26913e
kemin <ted1994511@gmail.com>
2022-04-17 16:07:35 +0800

    修改header version3

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ const port = process.env.PORT || 3000;
 server.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-server.use((req, res) => {
+server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
 
 server.use(bodyParser.json());
